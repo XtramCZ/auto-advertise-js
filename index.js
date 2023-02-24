d9bd01879fe930fc048a2f0dcc759fcd55f185fa
XtramCZ <m.herman.cb@gmail.com>
2023-02-24 17:39:05 +0100

    Fixed error on timeout

--- index.js
+++ index.js
@@ -25,25 +25,26 @@ console.log(color.red(`
     ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝     ╚═╝  ╚═╝╚═════╝ 
 
 `))
+
 const message = syncReadFile('./message.txt')
+async function sendMessage() {
+  for (let i = 0; i < config.channels.length; i++) {
+    const channel = client.channels.cache.get(config.channels[i])
+    try {
+      await channel.send(message)
+      config.debug_mode == 'true' ? console.log(` > A message was sent to "${channel.name}" in "${channel.guild.name}"`) : null
+    } catch (err) {
+      console.log(color.red(` > There was a problem sending a message to ${channel.name} in ${channel.guild.name}`))
+    }
+    await sleep(1000)
+  }
+  setTimeout(sendMessage, config.interval * 60000)
+}
 
 client.on('ready', async () => {
   console.log()
   console.log(color.green(` > Logged in`));
-  console.log('')
-
-
-async function sendMessage() {
-  for (let i = 0; i < config.channels.length; i++) {
-    try {
-      const channel = client.channels.cache.get(config.channels[i])
-      channel.send(message)
-      config.debug_mode == 'true' ? console.log(` > A message was sent to "${channel.name}" in "${channel.guild.name}"`) : null
-    } catch (err){console.log(color.red(` > There was a problem sending a message to ${channel.name} in ${channel.guild.name}`))}
-    await sleep(1000)
-  }
-  setTimeout(sendMessage, config.interval*60000)
-}
+  console.log('') 
 
 console.log(color.blue(' > Sending first batch of messages...'))
 sendMessage()
